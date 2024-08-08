07cf0637713a5e807e635c29cc1bf3e629cfd4f8
cat <cat@originull.org>
2024-08-07 20:22:38 -0700

    pipewire: resync from alpine

--- system/pipewire/pipewire-launcher.sh
+++ system/pipewire/pipewire-launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # We need to kill any existing pipewire instance to restore sound
-pkill -u "${USER}" -fx "/usr/bin/pipewire -c pipewire-pulse.conf" 1>/dev/null 2>&1
+pkill -u "${USER}" -fx /usr/bin/pipewire-pulse 1>/dev/null 2>&1
 pkill -u "${USER}" -fx /usr/bin/pipewire-media-session 1>/dev/null 2>&1
 pkill -u "${USER}" -fx /usr/bin/wireplumber 1>/dev/null 2>&1
 pkill -u "${USER}" -fx /usr/bin/pipewire 1>/dev/null 2>&1
@@ -10,7 +10,7 @@ exec /usr/bin/pipewire &
 
 # wait for pipewire to start before attempting to start related daemons
 while [ "$(pgrep -f /usr/bin/pipewire)" = "" ]; do
-        sleep 1
+	sleep 1
 done
 
 if [ -x /usr/bin/wireplumber ]; then
@@ -19,4 +19,4 @@ elif [ -x /usr/bin/pipewire-media-session ]; then
 	exec /usr/bin/pipewire-media-session &
 fi
 
-[ -f "/usr/share/pipewire/pipewire-pulse.conf" ] && exec /usr/bin/pipewire -c pipewire-pulse.conf &
+[ -f "/usr/share/pipewire/pipewire-pulse.conf" ] && exec /usr/bin/pipewire-pulse &
